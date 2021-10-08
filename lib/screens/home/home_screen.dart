e6e0e5cb7398271d3645929005a14b8b2d935ab4
Supriyadi <inyongmobile@gmail.com>
2021-10-08 18:25:59 +0700

    Nampilin foto profile di home pakai loading indicator

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -43,8 +43,19 @@ class _HomeScreenState extends State<HomeScreen> {
                 return Column(
                   children: [
                     SizedBox(
-                      height: 200,
-                      child: Image.network(state.data?.pict ?? ""),
+                      height: state.data == null ? 0 : 200,
+                      child: (state.data?.pict ?? "").contains("http")
+                          ? Image.network(
+                              state.data?.pict ?? "",
+                              loadingBuilder: (context, child,
+                                      loadingProgress) =>
+                                  loadingProgress == null
+                                      ? child
+                                      : const Center(
+                                          child: CircularProgressIndicator(),
+                                        ),
+                            )
+                          : SizedBox(),
                     ),
                     Text("user:\n${state.data?.toString()}"),
                   ],
