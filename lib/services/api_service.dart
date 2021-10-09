edd2bb1da943bd9a62c96f17b471297140d2aca2
Supriyadi <inyongmobile@gmail.com>
2021-10-09 13:21:22 +0700

    Revisi class ApiService

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,12 +1,56 @@
 import 'dart:io';
 
+import 'package:eproconnect/repositories/user_repository.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
+  static const baseUrl = "https://efpro.site";
+
+  //fungsi login
   Future<http.Response> login(
       {required String email, required String password}) {
-    return http.post(Uri.parse("https://efpro.site/api/auth/login"),
-        body: {"email": email, "password": password},
-        headers: {HttpHeaders.acceptHeader: "application/json"});
+    return doPost("/api/auth/login",
+        body: {"email": email, "password": password});
+  }
+
+  //mengambil daftar pelatihan
+  Future<http.Response> getPelatihanList() => doGet("/api/pelatihan");
+
+  //mengambil daftar pelatihan
+  Future<http.Response> getPropelList() => doGet("/api/auth/propel");
+
+  ////////////////////////////////////////////////////
+  Future<Map<String, String>> getDefaultHeader() async {
+    final token = await UserRepository().getUserToken();
+    return {
+      HttpHeaders.acceptHeader: "application/json",
+      HttpHeaders.authorizationHeader: "Bearer $token"
+    };
+  }
+
+  Future<http.Response> doPost(String path,
+      {Map<String, String> headers = const {},
+      body = Object,
+      String base = ""}) async {
+    final defaultHeaders = await getDefaultHeader();
+    defaultHeaders.addAll(headers);
+    if (base.isEmpty) base = baseUrl;
+    String url = "$base$path";
+    print("[POST]  =>$url");
+    print("[HEADER]=>$defaultHeaders");
+    print("[BODY]  =>$body");
+
+    return http.post(Uri.parse(url), body: body, headers: defaultHeaders);
+  }
+
+  Future<http.Response> doGet(String path,
+      {Map<String, String> headers = const {}, String base = ""}) async {
+    final defaultHeaders = await getDefaultHeader();
+    defaultHeaders.addAll(headers);
+    if (base.isEmpty) base = baseUrl;
+    String url = "$base$path";
+    print("[GETT]  =>$url");
+    print("[HEADER]=>$defaultHeaders");
+    return http.get(Uri.parse(url), headers: defaultHeaders);
   }
 }
